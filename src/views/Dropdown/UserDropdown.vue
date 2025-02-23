e21fbad9a7d53b3bfa7aa3b9b0f128eae99724f8
Lan <s35241607s@gmail.com>
2025-02-23 19:56:30 +0800

    feat: 在用戶下拉選單中新增用戶頭像顯示，並處理圖片加載錯誤

--- src/views/Dropdown/UserDropdown.vue
+++ src/views/Dropdown/UserDropdown.vue
@@ -3,11 +3,12 @@ import { onMounted, ref } from 'vue'
 import userService from '@/api/services/userService'
 import type { UserResponse } from '@/api/services/userType'
 import { useApi } from '@/composables/useApi'
+import avatar1 from '@images/avatars/avatar-1.png'
 
 const search = ref<string>('')
 const { data: users, loading: usersLoading, fetch: fetchUsers } = useApi<UserResponse>()
 
-const apiGatewayUrl = import.meta.env.VITE_API_GATEWAY_URL
+const apiGatewayUrl: string = import.meta.env.VITE_API_GATEWAY_URL
 
 onMounted(async () => {
   await fetchUsers(userService.getUsers())
@@ -26,10 +27,19 @@ onMounted(async () => {
     <template #item="{ props, item }">
       <VListItem
         v-bind="props"
-        :prepend-avatar="`${apiGatewayUrl}/api/v1/users/${item.value}/image`"
         :title="item.raw.username"
         :subtitle="item.raw.email"
-      />
+      >
+        <template #prepend>
+          <VAvatar>
+            <VImg :src="`${apiGatewayUrl}/api/v1/users/${item.value}/image`">
+              <template #error>
+                <VImg :src="avatar1" />
+              </template>
+            </VImg>
+          </VAvatar>
+        </template>
+      </VListItem>
     </template>
   </VAutocomplete>
 </template>
